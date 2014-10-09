fbcbda5a69f554a05189a5e89b5d306fc8c3c76f
Florian Golemo <fgolemo@gmail.com>
2014-10-09 12:27:59 +0200

    added HyperNEAT dummy

---
+++ HyperNEATdummy.py
@@ -0,0 +1,23 @@
+# HyperNEAT dummy
+import sys
+
+if (len(sys.argv) < 5):
+	print("Error, need the parameters: -I parameters.dat -O targetID -ORG [parent1 [parent2]]")
+	exit()
+
+id = sys.argv[4]
+genotypeFile = id + "-genotype.xml"
+voxFile = id + "-vox.vxa"
+print("here HN will print random strings\nline by line\ncontaining some debugging infos")
+
+print("generating 2 files:\n"+genotypeFile+" & "+voxFile)
+
+fileG = open(genotypeFile, "w")
+fileG.write("HyperNEAT dummy genotype XML file\n")
+fileG.close()
+
+fileV = open(voxFile, "w")
+fileV.write("HyperNEAT dummy Voxelyze VXA file\n")
+fileV.close()
+
+print("done")
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ vcmain
 ======
 
 Control script for the experiments of the Evolutionary Computing 2014 group, controlling HyperNEAT and Voxelyze workers on the Lisa.surfsara cluster.
+
+
+**Usage of the HN Dummy**
+python HyperNEATdummy.py -I test.dat -O IamAchildID -ORG parent1 parent2
