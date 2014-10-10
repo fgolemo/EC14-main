91d9e5b65768a3e5bcfd9c3f4280d34c7faf6001
Marjeta Markovic <markovic.marjeta@gmail.com>
2014-10-10 09:40:25 -0400

    Backtrackig 3rd versions.

--- EC14_main.py
+++ EC14_main.py
@@ -1,30 +1,105 @@
-#Python main script
-
-init_pop_size = 100
-sim_time_limit = 100 #seconds
-
-def create_init_population():
-	for (i=0 to init_pop_size):
-		individual_xml_file = run_hyperneat()
-		save_xml_to_pop_folder(individual_xml_file)
-		id = get_individual_id(individual_xml_file)
-		db_add_new_individual(id)
+# Python main script
+from subprocess import Popen
+import os.path
+import time
+import io
+# from threading import Thread # Not necessary, both workers are very light.
 
 
-db_exists = db_check_if_exists()
-if (!db_exists):
-	db_create()
-	create_init_population()
+#TODO write out all the other python files to be called
+# import GetNextChildren.py
+# import FillNewRobotLocationData.py
+# import CreateTables.py
+# import CloseRobotsArena.py
+# import CloseRobots.py
+
+#TODO include preprocessing.py somewhere - figure out how to do that: 1. From VoxCad output [difficult] 2. When VoxCad worker processes the individual [easier]
 
 
-spawn_voxlize_worker()
-spawn_hyperneat_worker()
 
-while(true)
-	sleep(1 second)
-	count = db_get_individuals_that_arent_HNed_or_processed()
-	if (count == 0)
-		kill_voxelize_worker()
-		kill_hyperneat_worker()
-		print ("done")
-		exit()
+# Ask user for experiment name:
+exp_name = raw_input("Please name your experiment: ") #TODO Make sure no special characters are used (except for - and _)
+
+#Check if file name is taken:
+while os.path.isdir(exp_name) == True:
+	exp_name = raw_input("That name is taken, please try again:")
+
+exp_path = os.path.expanduser("./"+exp_name+"/")
+
+# Ask user to define pop time and simulation time limit
+
+init_pop_size = get_int("Initial population size [100]: ")
+
+print "Population size set at " + init_pop_size
+
+endtime = get_int("Experiment time limit (in seconds) [100]:")
+
+print "Time limit set at " + sim_time_limit
+
+# Ask user if they are continuing a previous experiment
+db_given = raw_input("Are you continuing with an existing database? [Y/N]")
+
+while db_given != "Y" or "N":
+    print "That is not a valid answer.",
+    db_given = raw_input("Are you continuing with an existing database (type Y for yes and N for no)?")
+
+# Ask user to specify population .xml file path and making it writable to:
+if db_given == "Y":
+    pop_path = raw_input("Path to population XML folder (e.g. ~/Experiment1/Pop_files/:"))
+    while not os.path.exists(pop_path):
+        pop_path = raw_input("I can't find that folder, please try again:") 
+
+# Copy script files into experiment directory #TODO make sure files are not duplicates or overwritten
+files = glob.iglob("./*.py")
+os.makedirs(exp_path+"Scripts/")
+for file in files:
+    if os.path.isfile(file):
+        shutil.copy(file, exp_path+"Scripts/")
+		
+# If starting a new experiment, initialise population and empty database:
+if db_given == "N":	
+    pop_path = os.path.expanduser(exp_path+"Pop_files/")
+    if not os.path.exists(pop_path): 
+        os.makedirs(pop_path)
+    create_init_population()
+    CreateTables()
+
+# Call workers
+vox_worker = execfile("voxelize_worker.py")
+hyp_worker = execfile("hyperneat_worker.py")
+
+
+# If there are no individuals to be created or processed and there are no running jobs in lisa, send terminate request to the workers:
+while True:
+	time.sleep(2) # Delay for 2 seconds
+	count = # Number of individuals not yet HNed or virtualized (get from db) #TODO
+	if count == 0:
+		running_proc = # Check on running processes in Lisa (Check on jobs: "showq -u jheinerm") #TODO - Need to translate lisa output into bool
+		if running_proc == 0:
+		    vox_worker.terminate()
+		    hyp_worker.terminate()
+		    print "Done"
+		    exit()
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+################## Minor Functions ###################
+# Function to call in case the workers need to be stopped ABRUPTLY
+processes = [vox_worker,hyp_worker]
+def kill_subprocesses():
+    for item in processes:
+        item.kill()
+
+# Function to make sure input is an integer
+def get_int(msg):
+    while True:
+        line = raw_input(msg) or 100
+        try:
+            return int(line)
+        except ValueError:
+            print line, "is not a valid number"
--- voxelyze_worker.py
+++ voxelyze_worker.py
@@ -1,31 +1,43 @@
+import MySQLdb
+mdb = MySQLdb.connect(...)
+cur = mdb.cursor()
 #Python script for Voxelize worker... runs until cancelled
-
-# insert while here
-indiv = db_get_unprocessed_indiv() #gives back array/tuple with
-#ID, birthTime, hasBeenSimulated = 0, hasBeenProcessed = 0, hasBeenHNed = 0
 
 voxelize_queue = []
 waiting_time = 0
 queue_length = 12
 max_waiting_time = 60 * 60 # 60seconds * 60min = 1 hour in seconds
 
-while (true): # so basically run until cancelled
-	if (indiv == NULL)
+
+while True:
+    
+    GetNextChildren(endtime) #gives back array/tuple with
+    #ID, birthTime, hasBeenSimulated = 0, hasBeenProcessed = 0, hasBeenHNed = 0
+
+	if GetNextChildren(endtime) == NULL:
 		continue
-
-	if (indiv[1] > sim_time_limit)
-		db_set_indiv_processed(indiv)
-		continue
-
-	voxelize_queue.push(indiv_file)
-
-	if (voxelize_queue.length == queue_length or waiting_time > max_waiting_time):
+		
+	else:
+	    for ind in children:
+            voxelize_queue.append("./"+str(exp_name)+"/"+str(ChildID))
+    	
+ 	if (voxelize_queue.length == queue_length or waiting_time > max_waiting_time):
 		waiting_time = 0
-		qsub_create_job(voxelize_queue)
-		for (i=0 to queue_length) # mark all idividuals that we just submitted to simulation as simulated
-			db_set_indiv_simulated(indiv)
+		
+		# TODO Spawn parallel processes instead of for loop
+		### TRY IN LISA ###
+#        proc_per_node = "#PBS -lnodes=1:ppn=12"
+#    	subprocess.Popen(proc_per_node.split(),stdout=STDOUT,shell=True)
+    	###################
+    	# Note: Not using scratch space because operations are not I/O intensive.
+    	
+		for x in voxelize_queue:
+		    subprocess.Popen(["gsub","voxelize","./VoxCad/"+voxelize_queue(x),"-lnodes=1:ppn=12"],cwd="./VoxCad",shell=True) #TODO Need to test, string input may be an issue since it isnt in STDIN
+    	  	
+    	voxelize_queue = []
 
-	db_set_indiv_processed(indiv)
-	sleep(1 second)
+    time.sleep(1)
 	waiting_time += 1 #add 1 sec waiting time
-	indiv = db_get_unprocessed_indiv()
+
+	
+
