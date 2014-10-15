c2d7ca092bb648a181462dd8e8034f42d06972a2
Florian Golemo <fgolemo@gmail.com>
2014-10-15 22:05:48 +0200

    restructured Marjeta's code into functions

--- EC14_main.py
+++ EC14_main.py
@@ -1,12 +1,11 @@
 # Python main script
 from subprocess import Popen
-import os.path
-import time
+import os.path, time, glob, shutil
 import io
 # from threading import Thread # Not necessary, both workers are very light.
 
 
-#TODO write out all the other python files to be called
+# TODO write out all the other python files to be called
 # import GetNextChildren.py
 # import FillNewRobotLocationData.py
 # import CreateTables.py
@@ -15,54 +14,94 @@ import io
 
 #TODO include preprocessing.py somewhere - figure out how to do that: 1. From VoxCad output [difficult] 2. When VoxCad worker processes the individual [easier]
 
+def askExperimentName():
+    """ asks the user for the experiment name/location
+    :return: string Path to the project
+    """
+    exp_name = raw_input("Please name your experiment: ")  #TODO Make sure no special characters are used (except for - and _)
+
+    #Check if file name is taken:
+    while exp_name == '' and os.path.isdir(exp_name) == True:
+        exp_name = raw_input("That name is taken or empty, please try again: ")
+
+    exp_path = os.path.expanduser("~/EC14-Exp-" + exp_name + "/")
+    return exp_path
 
 
-# Ask user for experiment name:
-exp_name = raw_input("Please name your experiment: ") #TODO Make sure no special characters are used (except for - and _)
+def askPopulationSize():
+    """ ask for population size
+    :return: integer Population size
+    """
+    init_pop_size = raw_input("Initial population size [100]: ")
+    if (init_pop_size == ''):
+        init_pop_size = 100
+    else:
+        init_pop_size = int(init_pop_size)
+    print "Population size set at " + str(init_pop_size)
+    return init_pop_size
 
-#Check if file name is taken:
-while os.path.isdir(exp_name) == True:
-	exp_name = raw_input("That name is taken, please try again:")
 
-exp_path = os.path.expanduser("./"+exp_name+"/")
+def askEndTime(): #TODO: this is currently the experiment time, but it should be the in-simulation time
+    """ ask for max script runtime
+    :return: integer Maximum script runtime
+    """
+    endtime = raw_input("Experiment time limit (in seconds) [100]: ")
+    if (endtime == ''):
+        endtime = 100
+    else:
+        endtime = int(endtime)
+    print "Time limit set at " + str(endtime) + "s"
+    return endtime
 
-# Ask user to define pop time and simulation time limit
 
-init_pop_size = get_int("Initial population size [100]: ")
+def askWorkingDir():
+    """ aks the user if there are existing files for this experiment
+    :return:
+    """
 
-print "Population size set at " + init_pop_size
+    yes = {'yes', 'y', 'ye'}
+    no = {'no', 'n', ''}
 
-endtime = get_int("Experiment time limit (in seconds) [100]:")
+    #Ask user if they are continuing a previous experiment
+    db_given = raw_input("Are you continuing with an existing database? [y/N]: ")
+    while db_given not in yes and db_given not in no:
+        print "That is not a valid answer.",
+        db_given = raw_input("Are you continuing with an existing database (type Y for yes and N for no)? ").lower()
 
-print "Time limit set at " + sim_time_limit
+    # Ask user to specify population .xml file path and making it writable to:
+    if db_given in yes:
+        base_path = raw_input("Path to population XML folder (e.g. ~/EC14-Experiment-1):")
+        while not os.path.exists(os.path.expanduser(base_path)):
+            base_path = raw_input("I can't find that folder, please try again:")
+    else:
+        # If starting a new experiment, initialise population, empty database and copy the scripts:
+        base_path = askExperimentName()
+        pop_path = base_path + "population/"
+        if not os.path.exists(pop_path):
+            os.makedirs(pop_path)
+        installFiles(base_path)
+        # create_init_population() //TODO
+        # CreateTables() //TODO
 
-# Ask user if they are continuing a previous experiment
-db_given = raw_input("Are you continuing with an existing database? [Y/N]")
+    print "Working directory: " + base_path
+    return base_path
 
-while db_given != "Y" or "N":
-    print "That is not a valid answer.",
-    db_given = raw_input("Are you continuing with an existing database (type Y for yes and N for no)?")
 
-# Ask user to specify population .xml file path and making it writable to:
-if db_given == "Y":
-    pop_path = raw_input("Path to population XML folder (e.g. ~/Experiment1/Pop_files/:"))
-    while not os.path.exists(pop_path):
-        pop_path = raw_input("I can't find that folder, please try again:") 
+def installFiles(base_path):
+    """ Copy script files into experiment directory
+    :return: void
+    """
 
-# Copy script files into experiment directory #TODO make sure files are not duplicates or overwritten
-files = glob.iglob("./*.py")
-os.makedirs(exp_path+"Scripts/")
-for file in files:
-    if os.path.isfile(file):
-        shutil.copy(file, exp_path+"Scripts/")
-		
-# If starting a new experiment, initialise population and empty database:
-if db_given == "N":	
-    pop_path = os.path.expanduser(exp_path+"Pop_files/")
-    if not os.path.exists(pop_path): 
-        os.makedirs(pop_path)
-    create_init_population()
-    CreateTables()
+    files = glob.iglob("./*.py")
+    os.makedirs(base_path + "scripts/")
+    for file in files:
+        if os.path.isfile(file):
+            shutil.copy(file, base_path + "scripts/")
+
+base_path = askWorkingDir()
+end_time = askEndTime()
+pop_size = askPopulationSize()
+quit()
 
 # Call workers
 vox_worker = execfile("voxelize_worker.py")
@@ -71,29 +110,32 @@ hyp_worker = execfile("hyperneat_worker.py")
 
 # If there are no individuals to be created or processed and there are no running jobs in lisa, send terminate request to the workers:
 while True:
-	time.sleep(2) # Delay for 2 seconds
-	count = # Number of individuals not yet HNed or virtualized (get from db) #TODO
-	if count == 0:
-		running_proc = # Check on running processes in Lisa (Check on jobs: "showq -u jheinerm") #TODO - Need to translate lisa output into bool
-		if running_proc == 0:
-		    vox_worker.terminate()
-		    hyp_worker.terminate()
-		    print "Done"
-		    exit()
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
+    time.sleep(2)  # Delay for 2 seconds
+    count =  4# Number of individuals not yet HNed or virtualized (get from db) #TODO
+    if count == 0:
+        running_proc = 3# Check on running processes in Lisa (Check on jobs: "showq -u jheinerm") #TODO - Need to translate lisa output into bool
+        if running_proc == 0:
+            vox_worker.terminate()
+            hyp_worker.terminate()
+            print "Done"
+            exit()
+
+
+
+
+
+
+
+
 ################## Minor Functions ###################
 # Function to call in case the workers need to be stopped ABRUPTLY
-processes = [vox_worker,hyp_worker]
+processes = [vox_worker, hyp_worker]
+
+
 def kill_subprocesses():
     for item in processes:
         item.kill()
+
 
 # Function to make sure input is an integer
 def get_int(msg):
