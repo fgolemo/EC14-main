905b3219c01429fd12e37d94a8e0c71397e9ecce
Florian Golemo <fgolemo@gmail.com>
2014-10-25 21:06:09 +0200

    added parent detection, HN TODOs, Vox TODOs and tests

--- dbtest.py
+++ dbtest.py
@@ -1,6 +1,6 @@
 from db import DB
 
-db = DB("ec141:ec141@192.168.0.44/ec141")
+db = DB("ec141:ec141@192.168.0.44/ec141", 100, 50)
 db.dropTables()
 db.createTables()
 print(1)
@@ -17,7 +17,31 @@ traces = db.getTraces(id3)
 print(4)
 print(traces)
 
+toHN = db.getHNtodos()
+print(5)
+print(toHN)
 
+db.markAsHyperneated(toHN[0])
+
+toHN = db.getHNtodos()
+print(7)
+print(toHN)
+
+toVox = db.getVoxTodos()
+print(8)
+print(toVox)
+
+indiv1 = db.makeFakeBaby(1)
+indiv2 = db.makeFakeBaby(1,2)
+print(9)
+
+parents0 = db.getParents(1)
+parents1 = db.getParents(indiv1)
+parents2 = db.getParents(indiv2)
+print(10)
+print(parents0)
+print(parents1)
+print(parents2)
 
 
 print("done")
