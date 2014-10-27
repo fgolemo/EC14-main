5373badafc3597558465889dbf8e4a01aa741000
metamarkovic <markovic.marjeta@gmail.com>
2014-10-27 04:00:31 +0100

    hopefully bugfixes to scripts

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 module load stopos
-stopos create -p dead.${2}.pool
-stopos add -p dead.${2}.pool $1
+stopos create -p ~/dead.${2}.pool
+stopos add -p ~/dead.${2}.pool $1
 
 scriptdir=${HOME}/EC14-main/scripts
 
-JOB_ID=`qsub -o logs -e logs -v pool='dead.${2}.pool' ${scriptdir}/run_vox.sh`
+JOB_ID=`qsub -o logs -e logs -v pool=~/dead.${2}.pool ${scriptdir}/run_vox.sh`
 
 #display job queue
 showq -u $USER
