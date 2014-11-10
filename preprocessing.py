bbe4a0a809ff61b46f65c646da90e16f7473bb16
Jacqueline Heinerman <jheinerm@test3.irc.sara.nl.irc.sara.nl>
2014-11-10 21:43:25 +0100

    bugfixes for live operation

--- preprocessing.py
+++ preprocessing.py
@@ -54,13 +54,13 @@ class Preprocessor():
 
     def correctArenaInfinite(self, coordinates, arenaX, arenaY):
         if coordinates[2] < 0:
-            coordinates[2] = str(coordinates[2] + arenaX)
+            coordinates[2] = float(coordinates[2]) + arenaX
         if coordinates[2] > arenaX:
-            coordinates[2] = str(coordinates[2] - arenaX)
+            coordinates[2] = float(coordinates[2]) - arenaX
         if coordinates[3] < 0:
-            coordinates[3] = str(coordinates[3] + arenaY)
+            coordinates[3] = float(coordinates[3]) + arenaY
         if coordinates[3] > arenaY:
-            coordinates[3] = str(coordinates[3] - arenaY)
+            coordinates[3] = float(coordinates[3]) - arenaY
         return coordinates
 
     def correctArena(self, coordinates, arenaX, arenaY, arenaType):
