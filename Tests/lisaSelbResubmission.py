18a35a7726af5381a523475f3a46d900eaaa0857
Florian Golemo <fgolemo@gmail.com>
2014-11-16 13:56:14 +0100

    self-resubmit tests

---
+++ Tests/lisaSelbResubmission.py
@@ -0,0 +1,11 @@
+import sys, subprocess, time
+
+maxRuntime = 10
+currentRuntime = 0
+while currentRuntime < maxRuntime:
+    currentRuntime += 1
+    print("doing something...")
+    time.sleep(2)
+
+cmd = "qsub -o {logpath}.output.log -e {logpath}.error.log -l walltime=1799 'python {filename} {parameters}"
+subprocess.Popen(cmd.format(logpath = "~/.flo/lisaSelfResub", filename = __file__, parameters = " ".join(sys.argv[1:])), shell=True)
