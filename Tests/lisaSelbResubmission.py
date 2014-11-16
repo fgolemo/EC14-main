3d911393b4bf6544a4ba6ae94c5f186397edc484
Florian Golemo <fgolemo@gmail.com>
2014-11-16 21:30:10 +0100

    corrected test script for self-resubmission

--- Tests/lisaSelbResubmission.py
+++ Tests/lisaSelbResubmission.py
@@ -1,11 +1,22 @@
-import sys, subprocess, time
+import sys, subprocess, time, os
+
+print("current PID:" + str(os.getpid()) )
 
 maxRuntime = 10
 currentRuntime = 0
 while currentRuntime < maxRuntime:
-    currentRuntime += 1
+    currentRuntime += 2
     print("doing something...")
     time.sleep(2)
 
-cmd = "qsub -o {logpath}.output.log -e {logpath}.error.log -l walltime=4:59 'python {filename} {parameters}"
-subprocess.Popen(cmd.format(logpath = "~/.flo/lisaSelfResub", filename = __file__, parameters = " ".join(sys.argv[1:])), shell=True)
+cmd = "qsub -o {logpath}.output.log -e {logpath}.error.log -l walltime=4:59 -v file={file},params={params} {relhome}/.flo/lsresub.sh"
+home = os.environ.get('HOME')
+#home = subprocess.check_output("echo $HOME", shell=True)
+if (home == '/home/jheinerm'):
+    relhome = home
+else:
+    relhome = os.environ.get('PBS_O_HOME')
+
+qsub = subprocess.check_output(cmd.format(logpath = relhome + "/.flo/lisaSelfResub", file = os.path.basename(__file__), params = " ".join(sys.argv[1:]), relhome = relhome), shell=True)
+print(qsub)
+print("done")
---
+++ scripts/lisaSelbResubmission.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python ${PBS_O_HOME}/.flo/lsrtest.py $params#!/bin/bash
