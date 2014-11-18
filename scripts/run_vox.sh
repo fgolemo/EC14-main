1c647ef18089bee8abcd4231db37a9ab6c7fd981
Florian Golemo <fgolemo@gmail.com>
2014-11-18 20:17:26 +0100

    added scratch space usage and fixed a bug that might arise in parallel job processing

--- scripts/run_vox.sh
+++ scripts/run_vox.sh
@@ -6,17 +6,20 @@ cd $voxdir
 
 ncores=`sara-get-num-cores`
 
+mkdir "$TMPDIR"/vox
+
 for ((i=1; i<=ncores; i++)) ; do
 (
     stopos next -p ${pool}
     if [ "$STOPOS_RC" != "OK" ]; then # Parameter pool exhausted: we're done
         break
     fi
-    ./voxelyze -f ${population}${STOPOS_VALUE}_vox.vxa -p 100 > ${traces}duringVox/${STOPOS_VALUE}.trace
+    ./voxelyze -f ${population}${STOPOS_VALUE}_vox.vxa -p 100 > "$TMPDIR"/vox/${STOPOS_VALUE}.trace
+    echo "trace ${STOPOS_VALUE} done"
     stopos remove -p ${pool}
 ) &
 done
 
 wait
 
-mv ${traces}duringVox/* ${traces}afterVox/
+mv "$TMPDIR"/vox/* ${traces}afterVox/
