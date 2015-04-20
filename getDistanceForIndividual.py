8dd3fc4586e867e279f4bbcbc4385049939ba017
Florian Golemo <fgolemo@gmail.com>
2015-04-21 00:28:24 +0200

    added new skeleton for config reading and stuff and first test for distance script

---
+++ getDistanceForIndividual.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from EC14skeleton import Skeletor
+
+
+class DistanceCalc(Skeletor):
+
+    def __init__(self):
+        Skeletor.__init__(self)
+        self.individual = "0"
+        self.traces_after_pp_path = "traces_afterPP/"
+
+    def handleParams(self):
+        if len(sys.argv) != 2:
+            print(
+                "I take 2 argument: the configuration file for the experiment and an individual number/id, separated with a space")
+            quit()
+
+        self.configPath = sys.argv[1]
+        self.configPath = os.path.abspath(self.configPath)
+        self.individual = str(int(sys.argv[2]))
+
+    def readConfig(self, filename):
+        Skeletor.readConfig(self)
+        self.traces_after_pp_path = self.config.get('Postprocessing', 'traces_after_pp_path')
+
+    def calcDistance(self):
+        filename = self.base_path + self.traces_during_pp_path + self.individual + ".trace"
+        with open(filename, 'r') as inputFile:
+            distEuclidean = self.distanceEuclidian(inputFile)
+
+    def distanceEuclidian(self, inputFile):
+        for line in inputFile:
+            print line
+            quit()
+        
+
+if __name__ == "__main__":
+    dc = DistanceCalc()
+    dc.start()
+    dc.calcDistance()
+
