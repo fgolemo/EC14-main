c7ff99997250170816d8f898bb72f05e5bdcbb69
Florian Golemo <fgolemo@gmail.com>
2015-04-21 00:57:00 +0200

    added the actual code for the distance calculations

--- getDistanceForIndividual.py
+++ getDistanceForIndividual.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import math
 from EC14skeleton import Skeletor
 
 
@@ -27,16 +28,54 @@ class DistanceCalc(Skeletor):
     def calcDistance(self):
         filename = self.base_path + self.traces_after_pp_path + self.individual + ".trace"
         with open(filename, 'r') as inputFile:
-            distEuclidean = self.distanceEuclidian(inputFile)
+            distEuclideanStep = self.distanceStep(inputFile, "euclidean")
+            distManhattenStep = self.distanceStep(inputFile, "manhatten")
+            distEuclideanTotal = self.distanceTotal(inputFile, "euclidean")
+            distManhattenTotal = self.distanceTotal(inputFile, "manhatten")
+        return [distEuclideanStep, distManhattenStep, distEuclideanTotal, distManhattenTotal]
 
-    def distanceEuclidian(self, inputFile):
+    def distanceStep(self, inputFile, type):
+        firstRun = True
+        dist = 0
         for line in inputFile:
-            print line
-            quit()
-        
+            lineSplit = line.split("\t")
+            if not firstRun:
+                x_diff = x - float(lineSplit[2])
+                y_diff = y - float(lineSplit[3])
+                if type == "euclidean":
+                    dist += math.sqrt((x_diff**2) + (y_diff**2))
+                if type == "manhatten":
+                    dist += math.fabs(x_diff) + math.fabs(y_diff)
+
+            x = float(lineSplit[2])
+            y = float(lineSplit[3])
+            firstRun = False
+        return dist
+
+    def distanceTotal(self, inputFile, type):
+        firstRun = True
+        firstLine = []
+        lastLine = []
+        lineSplit = []
+        for line in inputFile:
+            lineSplit = line.split("\t")
+            if firstRun:
+                firstLine = lineSplit
+            else:
+                pass
+
+        lastLine = lineSplit
+        x_diff = float(firstLine[2]) - float(lastLine[2])
+        y_diff = float(firstLine[3]) - float(lastLine[3])
+        if type == "euclidean":
+            return  math.sqrt((x_diff**2) + (y_diff**2))
+        if type == "manhatten":
+            return math.fabs(x_diff) + math.fabs(y_diff)
+
 
 if __name__ == "__main__":
     dc = DistanceCalc()
     dc.start()
-    dc.calcDistance()
+    distances = dc.calcDistance()
+    print " ".join(distances)
 
