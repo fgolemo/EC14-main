6f22050246e8b5075ba159f53293866bb54e4856
Marjeta Markovic <markovic.marjeta@gmail.com>
2015-05-11 20:00:10 +0200

    remove old redundant code, simplify probability function

--- disease_parser.py
+++ disease_parser.py
@@ -8,7 +8,7 @@ import os
 
 class plagueVirus():
 
-    def concat(args):
+    def concat(self, args):
         string = ""
         for each in args:
             string += str(each)
@@ -24,40 +24,25 @@ class plagueVirus():
                 tree = ET.ElementTree(file="./population/" + dna_file)
                 root = tree.getroot()
                 layers = root.find('VXC').find('Structure').find('Data').findall('Layer')
-                probability = 0.1
+                probability = 1
 
                 dna_list = ""
                 for layer in layers:
                     dna_list = list(layer.text.strip())
-                    index_list = list(range(0,99))
-                    shuffle(index_list)
-                    mutations_count = 5
                     position_to_check = 0
-                    for i in range(mutations_count):
-                        found_mutation = False
-                        not_found = False
-
-                        while not found_mutation:
-                            index_number = index_list[position_to_check]
-                            tissue = dna_list[index_number]
-                            if tissue == '3' or tissue == '4':
-                                if random.random() < probability:
-                                    dna_list[index_number] = '2'
-                                    print "Atrophied a muscle at index " + str(index_number) + " to " + str(dna_list[index_number]) + "."
-                                else:
-                                    print "Muscle at index " + str(index_number) + " unchanged."
-                                    pass
-                                found_mutation = True
+                    for index in range(len(dna_list)):
+                        tissue = dna_list[index]
+                        if tissue == '3' or tissue == '4':
+                            if random.random() <= probability:
+                                dna_list[index] = '-'
+                                print "Atrophied a muscle at index " + str(index) + " to " + str(dna_list[index]) + "."
                             else:
-                                position_to_check += 1
-                                # print "Trying next index.."
-                                if position_to_check == len(index_list):
-                                    not_found = True
-                                    # print "I could not find a muscle voxel!"
-                                    break
-                        if not_found:
-                            print "I could not find a muscle voxel!"
-                            break
+                                print "Muscle at index " + str(index) + " unchanged."
+                                pass
+
+                        else:
+                            continue
+
                     layer.text = self.concat(dna_list)
                     tree.write("./population/" + dna_file)
 
